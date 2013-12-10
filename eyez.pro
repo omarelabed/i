5f4a681bcb5a0fe0fc2c9fbdf5f9c652f22be58e
omarelabed <omar@omars-air-2.mobile.usilu.net>
2013-12-10 14:48:24 +0100

    libraries + webcam tracing
    updated library dependencies and added webcam tracing

--- eyez.pro
+++ eyez.pro
@@ -28,7 +28,11 @@ TEMPLATE = app
 DEPENDPATH += .
 INCLUDEPATH += .
 #INCLUDEPATH += /usr/local/include
-QT += core gui xml opengl
+INCLUDEPATH += "/opt/local/include/"
+LIBS += "/opt/local/lib/libopencv*.dylib"
+#LIBS += "/usr/local/lib/libopencv*.dylib"
+#QT += core gui xml opengl
+QT += opengl
 
 OBJECTS_DIR = build
 MOC_DIR = mocs
@@ -49,25 +53,25 @@ TARGET = eyez
 
 TEMPLATE = app
 
-unix|win32: LIBS += -lopencv_calib3d
-unix|win32: LIBS += -lopencv_stitching
-unix|win32: LIBS += -lopencv_calib3d
-unix|win32: LIBS += -lopencv_contrib
-unix|win32: LIBS += -lopencv_core
-unix|win32: LIBS += -lopencv_features2d
-unix|win32: LIBS += -lopencv_flann
-unix|win32: LIBS += -lopencv_gpu
-unix|win32: LIBS += -lopencv_highgui
-unix|win32: LIBS += -lopencv_imgproc
-unix|win32: LIBS += -lopencv_legacy
-unix|win32: LIBS += -lopencv_ml
-unix|win32: LIBS += -lopencv_nonfree
-unix|win32: LIBS += -lopencv_objdetect
-unix|win32: LIBS += -lopencv_photo
-unix|win32: LIBS += -lopencv_stitching
-unix|win32: LIBS += -lopencv_superres
-unix|win32: LIBS += -lopencv_video
-unix|win32: LIBS += -lopencv_videostab
+#unix|win32: LIBS += -lopencv_calib3d
+#unix|win32: LIBS += -lopencv_stitching
+#unix|win32: LIBS += -lopencv_calib3d
+#unix|win32: LIBS += -lopencv_contrib
+#unix|win32: LIBS += -lopencv_core
+#unix|win32: LIBS += -lopencv_features2d
+#unix|win32: LIBS += -lopencv_flann
+#unix|win32: LIBS += -lopencv_gpu
+#unix|win32: LIBS += -lopencv_highgui
+#unix|win32: LIBS += -lopencv_imgproc
+#unix|win32: LIBS += -lopencv_legacy
+#unix|win32: LIBS += -lopencv_ml
+#unix|win32: LIBS += -lopencv_nonfree
+#unix|win32: LIBS += -lopencv_objdetect
+#unix|win32: LIBS += -lopencv_photo
+#unix|win32: LIBS += -lopencv_stitching
+#unix|win32: LIBS += -lopencv_superres
+#unix|win32: LIBS += -lopencv_video
+#unix|win32: LIBS += -lopencv_videostab
 
 HEADERS += \
     Base.h \
--- main.cpp
+++ main.cpp
@@ -52,10 +52,142 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv/cvaux.hpp>
 
+IplImage* GetThresholdedImage(IplImage* img)
+{
+    // Convert the image into an HSV image
+    IplImage* imgHSV = cvCreateImage(cvGetSize(img), 8, 3);
+    cvCvtColor(img, imgHSV, CV_BGR2HSV);
+
+    IplImage* imgThreshed = cvCreateImage(cvGetSize(img), 8, 1);
+
+    // Values 20,100,100 to 30,255,255 working perfect for yellow at around 6pm
+    cvInRangeS(imgHSV, cvScalar(0, 0, 250), cvScalar(0, 0, 360), imgThreshed);
+
+    cvReleaseImage(&imgHSV);
+
+    return imgThreshed;
+}
 
 //! [0]
 int main(int argc, char *argv[])
 {
+    // Initialize capturing live feed from the camera
+    CvCapture* capture = 0;
+    capture = cvCaptureFromCAM(0);
+
+    // Couldn't get a device? Throw an error and quit
+    if(!capture)
+    {
+        printf("Could not initialize capturing...\n");
+        return -1;
+    }
+
+    // The two windows we'll be using
+    cvNamedWindow("video");
+    cvNamedWindow("thresh");
+
+    // This image holds the "scribble" data...
+    // the tracked positions of the ball
+    IplImage* imgScribble = NULL;
+
+    // An infinite loop
+    while(true)
+    {
+        // Will hold a frame captured from the camera
+        IplImage* frame = 0;
+        frame = cvQueryFrame(capture);
+
+        // If we couldn't grab a frame... quit
+        if(!frame)
+            break;
+
+        // If this is the first frame, we need to initialize it
+        if(imgScribble == NULL)
+        {
+            imgScribble = cvCreateImage(cvGetSize(frame), 8, 3);
+        }
+
+        cv::Size frameSize = cvGetSize(frame);
+
+        // Holds the yellow thresholded image (yellow = white, rest = black)
+        IplImage* imgYellowThresh = GetThresholdedImage(frame);
+
+        // Calculate the moments to estimate the position of the ball
+        CvMoments *moments = (CvMoments*)malloc(sizeof(CvMoments));
+        cvMoments(imgYellowThresh, moments, 1);
+
+        // The actual moment values
+        double moment10 = cvGetSpatialMoment(moments, 1, 0);
+        double moment01 = cvGetSpatialMoment(moments, 0, 1);
+        double area = cvGetCentralMoment(moments, 0, 0);
+
+        // Holding the last and current ball positions
+        static int posX = 0;
+        static int posY = 0;
+
+        int lastX = posX;
+        int lastY = posY;
+
+        posX = moment10/area;
+        posY = moment01/area;
+
+        double relX = (double)posX/(double)frameSize.width;
+        double relY = (double)posY/(double)frameSize.height;
+
+        // Print it out for debugging purposes
+        printf("position (%d,%d)", posX, posY);
+        printf(" -- relative posn: (%f,%f)\n", relX, relY);
+
+        // We want to draw a line only if its a valid position
+        if(lastX>0 && lastY>0 && posX>0 && posY>0)
+        {
+            // Draw a yellow line from the previous point to the current point
+            cvLine(imgScribble, cvPoint(posX, posY), cvPoint(lastX, lastY), cvScalar(0,255,255), 5);
+        }
+
+        // Add the scribbling image and the frame... and we get a combination of the two
+        cvAdd(frame, imgScribble, frame);
+        cvShowImage("thresh", imgYellowThresh);
+        cvShowImage("video", frame);
+
+        // Wait for a keypress
+        int c = cvWaitKey(10);
+        if(c!=-1)
+        {
+            // If pressed, break out of the loop
+            break;
+        }
+
+        // Release the thresholded image... we need no memory leaks.. please
+        cvReleaseImage(&imgYellowThresh);
+
+        delete moments;
+    }
+
+    // We're done using the camera. Other applications can now use it
+    cvReleaseCapture(&capture);
+//    return 0;
+//    cv::VideoCapture camera;
+//    camera.open(0);
+//    if (!camera.isOpened()){
+//        std::cerr<<"Couldn't open cam"<<std::endl;
+//        exit(1);
+//    }
+//    camera.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+//    camera.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+
+//    while (true) {
+//        cv::Mat cameraFrame;
+//        camera>>cameraFrame;
+//        if (cameraFrame.empty()){
+//            std::cerr<<"No frame from camera"<<std::endl;
+//            exit(1);
+//        }
+//        imshow("Camera", cameraFrame);
+
+//        char keypress = cv::waitKey(20);
+//        if (keypress==27) break;
+//    }
 
     QApplication app(argc, argv);
     GLRender viewer( 0, Qt::Window );
